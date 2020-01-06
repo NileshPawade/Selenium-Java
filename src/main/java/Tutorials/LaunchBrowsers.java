02f9d1d286c94d1df60b186b64fa531601ad1461
NileshPawade <nspawade74@gmail.com>
2020-01-06 11:17:26 +0530

    chrome launch assignment added
    Selected user diretcory path dynamically and upload successfully

--- src/main/java/Tutorials/LaunchBrowsers.java
+++ src/main/java/Tutorials/LaunchBrowsers.java
@@ -8,18 +8,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class LaunchBrowsers {
 	public static void main(String[] args) {
 		
-		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\Neosoft\\eclipse-workspace\\CompleteSelenium\\src\\main\\java\\WebContents\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+
+				"\\src\\main\\java\\Webcontents\\chromedriver.exe");
 
 		WebDriver driver = new ChromeDriver();
 
-		driver.get("https://www.flipkart.com/mobiles/mi~brand/pr?sid=tyy,4io&otracker=nmenu_sub_Electronics_0_Mi");
+		driver.get("https://red.purestudy.com");
 
 		driver.manage().window().maximize();
 
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		
+		driver.close();
 		
 	}
 
